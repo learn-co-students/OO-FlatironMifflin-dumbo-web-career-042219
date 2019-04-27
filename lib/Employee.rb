89900cfc54e4b03d60cd1d9923f64bc8e904b130
Miles Marillo <mjm03@hampshire.edu>
2019-04-26 23:24:19 -0400

    Done.

--- lib/Employee.rb
+++ lib/Employee.rb
@@ -1,3 +1,46 @@
 class Employee
 
+attr_accessor :name, :salary, :manager
+
+@@all = []
+
+	def initialize(name, salary, manager)
+		@name = name
+		@salary = salary	
+		@manager = manager
+		@@all << self
+	end
+
+	def manager_name
+		self.manager.name
+	end
+
+	def self.all
+		@@all
+	end
+
+	def self.paid_over(amount)
+	arr = Employee.all.select do |employee|
+		  employee.salary >= amount
+	end
+	arr.map do  |empl| 
+	empl.name 
+	end
+end
+
+	def self.find_by_department(search)
+	arr =	Manager.all.find do |manager|
+			manager.department == search
+	end
+	arr.name
+	end
+
+	def tax_bracket
+	# new_arr = [(self.salary - 1000) ... (self.salary + 1000)]
+	@@all.select do |tax_employee|
+	if tax_employee.salary.between?((self.salary - 1000),(self.salary + 1000))
+		tax_employee.name
+	end
+end
+end
 end
--- lib/Manager.rb
+++ lib/Manager.rb
@@ -1,3 +1,37 @@
 class Manager
 
+	attr_accessor :name, :employees, :department, :age
+
+	@@all = []
+
+def initialize(name, department, age)
+	@name = name
+	@department = department
+	@age = age
+	@employees = []
+	@@all << self
+end
+
+def self.all
+	@@all
+end
+
+def hire_employee(name, salary)
+	new_employee = Employee.new(name, salary, self)
+	@employees << new_employee
+end
+
+def self.all_departments
+	@@all.map do |all|
+		all.department
+end
+end
+
+def self.average_age
+	all_ages =	all.map do |all|
+	all.age 
+end
+all_ages.sum.fdiv(all_ages.size)
+end
+
 end
