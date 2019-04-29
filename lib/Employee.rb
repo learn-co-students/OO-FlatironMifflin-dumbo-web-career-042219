8e6c309e2f2ab439a6a98f5f981481520be2eaf5
Aaron Chu <aaronchu357@gmail.com>
2019-04-29 18:25:42 -0400

    Done

--- lib/Employee.rb
+++ lib/Employee.rb
@@ -1,3 +1,30 @@
 class Employee
 
+  attr_accessor :name, :salary, :manager
+
+  @@employees = []
+
+  def initialize(name, salary, manager)
+    @name = name
+    @salary = salary
+    @manager = manager
+    @@employees << self
+  end
+
+  def self.all
+    @@employees
+  end
+
+  def self.paid_over(indiv_salary) #FIXNUM
+    self.all.select {|employee| employee.salary > indiv_salary}
+  end
+
+  def self.find_by_department(department) #STRING
+    self.all.find {|employee| employee.manager.department == department}
+  end
+
+  def tax_bracket
+    Employee.all.select {|employee| employee.salary <= self.salary + 1000 && employee.salary >= self.salary - 1000}
+  end
+
 end
--- lib/Manager.rb
+++ lib/Manager.rb
@@ -1,3 +1,39 @@
 class Manager
 
+  attr_accessor :name, :department, :age
+
+  @@all = []
+
+  def initialize(name, department, age)
+    @name = name
+    @department = department
+    @age = age
+    @@all << self
+  end
+
+  def employees
+    Employee.all.select {|employee| employee.manager == self}
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.all_departments
+    self.all.map {|manager| manager.department}
+  end
+
+  def self.average_age
+    ages = self.all.map {|manager| manager.age}
+    total = 0
+    ages.each {|age| total += age}
+    total.to_f / ages.length.to_f
+  end
+
+  def hire_employee(name, salary)
+    employees << Employee.new(name, salary, self)
+  end
+
+
+
 end
