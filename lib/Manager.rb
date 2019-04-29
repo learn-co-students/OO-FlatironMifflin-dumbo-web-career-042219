7b1d3f6715c54ce0cda8cffe934ac2f886b856ad
github username <github email address>
2019-04-29 16:45:23 -0400

    Done.

--- lib/Manager.rb
+++ lib/Manager.rb
@@ -1,3 +1,44 @@
+require 'pry'
+
 class Manager
+
+  attr_accessor :name, :department, :age, :employee, :salary
+
+  @@all = []
+
+  def initialize(name, department, age)
+    @name = name
+    @department = department
+    @age = age
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def employees
+    Employee.all.select do |employee|
+      employee.manager == self
+    end
+  end
+
+  def hire_employee(employee, salary)
+    employees << Employee.new(employee,salary, self)
+  end
+
+  def self.all_departments
+    self.all.map do |manager|
+      manager.department
+    end
+  end
+
+  def self.average_age
+    total = 0
+    self.all.map do |manager_age|
+      total += manager_age.age
+    end
+    average = total / self.all.length
+  end
 
 end
