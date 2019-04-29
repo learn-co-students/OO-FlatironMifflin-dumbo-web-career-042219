49e68de86b0941472c1523af69e4d986ebae66af
Wei <theweifeng@gmail.com>
2019-04-29 18:33:20 -0400

    Done.

--- lib/Manager.rb
+++ lib/Manager.rb
@@ -1,3 +1,36 @@
 class Manager
+  attr_reader :name, :age
+  attr_accessor :department
 
+  @@managers = []
+  
+  def initialize(name, department, age)
+    @name = name
+    @department = department
+    @age = age
+    self.class.all << self
+  end
+
+  def self.employees
+    Employees.all.select {|employee| employee.manager == self}
+  end
+
+  def self.all
+    @@managers
+  end
+
+  def hire_employee(name, salary)
+    employee = Employee.new(name, salary)
+    employee.manager = self
+  end
+
+  def self.all_departments
+    self.all.map {|manager| manager.department}
+  end
+
+  def self.average_age
+    arr = self.all.map {|manager| manager.age}
+
+    arr.sum/(arr.count * 1.0)
+  end
 end
